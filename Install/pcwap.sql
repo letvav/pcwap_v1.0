7119530e8e028dde1c9b1dd00e55a75989e68ce9
pcwap <azanbbs@gmail.com>
2014-10-10 21:44:54 +0800

    Revert "2144"
    This reverts commit 9a05d859a7043205df27730fc7fd6db21195616a.

--- Install/pcwap.sql
+++ Install/pcwap.sql
@@ -1,53 +1,53 @@
 
 CREATE TABLE IF NOT EXISTS `az_ads` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `adurl` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `adpic` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `adremak` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `adurl` varchar(255) DEFAULT NULL,
+  `adpic` varchar(255) DEFAULT NULL,
+  `adremak` varchar(255) DEFAULT NULL,
   `isshow` smallint(1) unsigned DEFAULT '1',
-  `time` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `az_admin` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `pwd` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `author` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
   `status` smallint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `az_book` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `tel` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `content` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `reply` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `tel` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `reply` varchar(255) DEFAULT NULL,
   `status` tinyint(1) DEFAULT '0',
   `infoid` int(11) DEFAULT NULL,
-  `time` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `az_cate` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `catename` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `url` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `catetitle` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `catekey` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `catedesc` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `content` text CHARACTER SET utf8 NOT NULL,
-  `catetemp` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `infotemp` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `catepage` int(4) unsigned  DEFAULT NULL,
-  `catelogo` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `catename` varchar(255) DEFAULT NULL,
+  `url` varchar(255) NOT NULL,
+  `catetitle` varchar(255) DEFAULT NULL,
+  `catekey` varchar(255) DEFAULT NULL,
+  `catedesc` varchar(255) DEFAULT NULL,
+  `content` text NOT NULL,
+  `catetemp` varchar(255) DEFAULT NULL,
+  `infotemp` varchar(255) DEFAULT NULL,
+  `catepage` int(4) unsigned DEFAULT NULL,
+  `catelogo` varchar(255) DEFAULT NULL,
   `catetype` int(10) unsigned DEFAULT '0',
   `pid` int(10) DEFAULT '0',
   `sort` int(6) DEFAULT '100',
   `menu` smallint(1) unsigned NOT NULL DEFAULT '1',
-  `outurl` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `outurl` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `pid` (`pid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
@@ -61,30 +61,30 @@ CREATE TABLE IF NOT EXISTS `az_diy` (
   `status` smallint(1) NOT NULL DEFAULT '1',
   `pid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 CREATE TABLE IF NOT EXISTS `az_info` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `title` varchar(255) CHARACTER SET utf8 DEFAULT '',
-  `key` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `desc` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `content` text CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT '',
+  `key` varchar(255) DEFAULT NULL,
+  `desc` varchar(255) DEFAULT NULL,
+  `content` text NOT NULL,
   `cid` int(4) unsigned NOT NULL,
   `istop` smallint(1) unsigned DEFAULT '0',
   `isrec` smallint(1) unsigned DEFAULT '0',
   `isshow` smallint(1) unsigned DEFAULT '0',
   `isrev` smallint(1) DEFAULT '0',
   `ispic` smallint(1) DEFAULT '0',
-  `pic` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `pic` varchar(255) DEFAULT NULL,
   `hits` int(11) unsigned DEFAULT '0',
-  `author` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `price` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
+  `author` varchar(255) DEFAULT NULL,
+  `price` varchar(20) DEFAULT NULL,
   `revs` int(11) unsigned DEFAULT '0',
-  `pcs` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `time` varchar(20) CHARACTER SET utf8 DEFAULT NULL,
-  `temp` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `pcs` varchar(255) DEFAULT NULL,
+  `time` varchar(20) DEFAULT NULL,
+  `temp` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cid` (`cid`) USING BTREE,
   KEY `isshow` (`isshow`) USING BTREE,
@@ -95,12 +95,12 @@ CREATE TABLE IF NOT EXISTS `az_info` (
 
 CREATE TABLE IF NOT EXISTS `az_links` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `key` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
-  `remak` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `key` varchar(255) DEFAULT NULL,
+  `remak` varchar(255) DEFAULT NULL,
   `sort` int(10) unsigned DEFAULT NULL,
   `isshow` smallint(1) DEFAULT '1',
-  `time` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `time` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -114,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `az_search` (
   `status` tinyint(1) DEFAULT '0',
   `time` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 CREATE TABLE IF NOT EXISTS `az_tags` (
@@ -124,7 +124,7 @@ CREATE TABLE IF NOT EXISTS `az_tags` (
   `hits` int(10) unsigned DEFAULT '1',
   `url` varchar(255) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 
@@ -133,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `az_weixin_appid` (
   `secret` varchar(255) CHARACTER SET utf8 NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 INSERT INTO `az_weixin_appid` (`appid`, `secret`, `id`) VALUES
 ('465465', '654654', 1);
@@ -149,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `az_weixin_gz` (
   `sorts` int(11) DEFAULT NULL,
   `star` tinyint(1) unsigned DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 
@@ -164,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `az_weixin_huifu` (
   `sorts` int(11) DEFAULT NULL,
   `star` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 CREATE TABLE IF NOT EXISTS `az_weixin_menu` (
@@ -176,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `az_weixin_menu` (
   `pid` int(11) DEFAULT NULL,
   `sorts` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 
 
